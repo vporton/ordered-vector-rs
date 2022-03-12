b9e60dda19bc66a506a79059eb4acda63083e8fe
Victor Porton <porton.victor@gmail.com>
2022-03-13 01:46:45 +0200

    added sort by provided order

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
-use bisection::bisect_left;
+use std::cmp::Ordering;
+use std::ops::Index;
+use bisection::bisect_left_by;
 
-trait Resort<T> {
+trait Resort<T> where Self: Index<usize, Output = T> {
     /// Sort again already sorted sequence after the element at `index` changed.
-    fn resort_element(&mut self, index: usize);
+    fn resort_element(&mut self, index: usize)
+        where Self::Output: Ord + Sized
+    {
+        self.resort_element_by(index, |e, value| T::cmp(e, &value));
+    }
+    fn resort_element_by<F>(&mut self, index: usize, f: F)
+        where F: FnMut(&T, &T) -> Ordering;
 }
 
 impl<T: Ord> Resort<T> for Vec<T> {
     // TODO: It can be made more efficient.
-    fn resort_element(&mut self, index: usize) {
+    fn resort_element_by<F>(&mut self, index: usize, mut f: F)
+        where F: FnMut(&T, &T) -> Ordering
+    {
         let value = self.remove(index);
-        let new_index = bisect_left(self.as_slice(), &value);
+        let new_index = bisect_left_by(self.as_slice(), |e| f(e, &value));
         self.insert(new_index, value);
     }
 }
